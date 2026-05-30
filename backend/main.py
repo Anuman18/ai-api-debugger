adb02d24dc98a95e5049ceb13560d58a49c51ec4
Anuman Modi <anuman181105@gmail.com>
2026-05-30 14:22:58 +0530

    Fixed CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,9 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://ai-api-debugger-nu.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
