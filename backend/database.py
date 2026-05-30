380863a5c7d94962e0343f9ec7065ef3bd87e4b1
Anuman Modi <anuman181105@gmail.com>
2026-05-30 13:31:38 +0530

    Updated production database config

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,9 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://neondb_owner:npg_9BqYgSWUnaG7@ep-patient-rain-aqv1koks.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
