535998e2b6c45a6292b7c759cd01e01954005e12
Anuman Modi <anuman181105@gmail.com>
2026-05-30 13:28:30 +0530

    Added requirements.txt

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://anumanmodi@localhost/api_debugger"
+DATABASE_URL = "postgresql://neondb_owner:npg_9BqYgSWUnaG7@ep-patient-rain-aqv1koks.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 
